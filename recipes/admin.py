72ad0517934801944f335c2a57b428f91621e57e
PiotrPliszka <181106@student.uwm.edu.pl>
2026-03-13 17:38:40 +0100

    Zarejestrowanie Tabeli w Django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredients
 
 # Register your models here.
+
+
+class IngredeintAdmin(admin.ModelAdmin):
+    pass
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Ingredient, IngredeintAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredients, RecipeIngredientsAdmin)
