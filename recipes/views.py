83d3b37ec4608771141099e08bfaad23c30c6e53
PiotrPliszka <181106@student.uwm.edu.pl>
2026-03-13 20:03:40 +0100

    Dodanie opcjii filtrowania

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ from .models import Recipe, Ingredient, RecipeIngredients
 def index(request):
     all_recipes = Recipe.objects.all()
     return render(request, "recipes/index.html", {"all_recipes": all_recipes})
+
+
+def category(request, slug):
+    all_recipes = Recipe.objects.all()
+    sorted = all_recipes.filter(category=slug.capitalize()).select_related("author")
+    return render(request, "recipes/index.html", {"all_recipes": sorted})
--- slinotok/urls.py
+++ slinotok/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("recipes", include("recipes.urls")),
+    path("recipes/", include("recipes.urls")),
 ]
 
 if settings.DEBUG:
