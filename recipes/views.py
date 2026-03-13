990a4e08af47e827434b02e12ddd930545a8889c
PiotrPliszka <181106@student.uwm.edu.pl>
2026-03-13 19:19:56 +0100

    Fix

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,4 @@ from .models import Recipe, Ingredient, RecipeIngredients
 
 def index(request):
     all_recipes = Recipe.objects.all()
-    print(all_recipes[0].image.url)
     return render(request, "recipes/index.html", {"all_recipes": all_recipes})
